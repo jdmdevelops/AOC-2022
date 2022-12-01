2d8aeeee30dd0073f269f7d4951412e00bfed8c7
Justin Moore <jdmdevelops@gmail.com>
2022-12-01 16:49:59 -0500

    day1

--- Day1/solution.js
+++ Day1/solution.js
@@ -1,3 +1,25 @@
-const add = (a, b) => a + b
+const fs = require('fs')
+let input
+try {
+  input = fs.readFileSync('./input.txt', 'utf8')
+} catch (err) {
+  console.error(err)
+}
 
-console.log(add(5, 3))
+let largestValues = []
+
+const reducer = (accumulator, item) => {
+  if (item === '') {
+    if (largestValues.length < 3) largestValues.push(accumulator)
+    if (accumulator > largestValues[0]) largestValues.splice(0, 1, accumulator)
+    largestValues.sort((a, b) => a - b)
+    accumulator = 0
+  }
+  return accumulator + Number(item)
+}
+
+input.split(/\r?\n/).reduce(reducer, 0)
+const total = largestValues.reduce((accumulator, value) => {
+  return accumulator + value
+}, 0)
+console.log(total)
